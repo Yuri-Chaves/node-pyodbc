07ed369a45045ef09aa26f8bfde569d2d8e344df
Yuri-Chaves <yuri.chaves39@hotmail.com>
2025-06-20 08:18:08 -0300

    :sparkles: feat: Export ODBCError

--- dist/index.js
+++ dist/index.js
@@ -16,4 +16,5 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./src/interfaces"), exports);
 __exportStar(require("./src/odbcClient"), exports);
+__exportStar(require("./src/odbcError"), exports);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
-export * from './src/interfaces';
-export * from './src/odbcClient';
+export * from "./src/interfaces";
+export * from "./src/odbcClient";
+export * from "./src/odbcError";
